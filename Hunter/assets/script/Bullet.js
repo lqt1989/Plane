b4733a6bb7839b68a35d81471263b422382219e7
liuqingtao <393147374@qq.com>
2018-09-30 16:40:26 +0800

    add stone res

---
+++ Hunter/assets/script/Bullet.js
@@ -0,0 +1,34 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        speed:1,
+        addSpeed:0.25,
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        this.speed = 1
+        this.addSpeed = 0.5
+    },
+
+    start () {
+
+    },
+
+    update (dt) {
+        this.speed += this.addSpeed
+        this.node.y += this.speed;
+    },
+});
